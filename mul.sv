5d9c85f03328a7e80c57481b0ecda6f927827416
David Sheffield <sheffield.david@gmail.com>
2023-02-03 07:47:10 -0800

    pipelined - 3 cycle signed array multiplier. using modified baugh-wooley algorithm

--- mul.sv
+++ mul.sv
@@ -8,10 +8,21 @@ module csa(a,b,cin,s,cout);
    output [N-1:0] s;
    output [N-1:0] cout;
 
-   assign s = a ^ b ^ cin;
-   assign cout = a&b | (cin & (a ^ b));
+   wire [N-1:0]   w_xor_ab = a^b;
+   assign s = w_xor_ab ^ cin;
+   assign cout = a&b | (cin & w_xor_ab);
+endmodule // csa
 
-endmodule
+module ff(q,d,clk);
+   parameter N = 1;
+   input logic [N-1:0] d;
+   input logic 	       clk;
+   output logic [N-1:0] q;
+   always_ff@(posedge clk)
+     begin
+	q <= d;
+     end // always_ff@ (posedge clk)
+endmodule // dff
 
 module mul(clk,reset,opcode,go,
 	   src_A,
@@ -49,7 +60,6 @@ module mul(clk,reset,opcode,go,
    output logic 			   hilo_prf_ptr_val_out;
    output logic [`LG_HILO_PRF_ENTRIES-1:0] hilo_prf_ptr_out;
    
-   logic [63:0] 			   r_mul[`MUL_LAT:0];
    logic [`MUL_LAT:0] 			   r_complete;
    logic [`MUL_LAT:0] 			   r_do_madd;
    logic [`MUL_LAT:0] 			   r_do_msub;
@@ -60,8 +70,7 @@ module mul(clk,reset,opcode,go,
    logic [63:0] 		   r_madd[`MUL_LAT:0];
    logic [`LG_ROB_ENTRIES-1:0] 	   r_rob_ptr[`MUL_LAT:0];
   
-   
-   logic [63:0] 			   t_mul;
+   wire [63:0] 			   r_mul31;   
 
    assign complete = r_complete[`MUL_LAT];
    assign rob_ptr_out = r_rob_ptr[`MUL_LAT];
@@ -71,28 +80,43 @@ module mul(clk,reset,opcode,go,
    assign hilo_prf_ptr_val_out = r_hilo_val[`MUL_LAT];
    assign hilo_prf_ptr_out = r_hilo_ptr[`MUL_LAT];
 
+   logic 			   t_signed;
+   
    always_comb
      begin
-	y = r_mul[`MUL_LAT];
-	if(r_do_madd[`MUL_LAT])
-	  begin
-	     y = r_mul[`MUL_LAT] + r_madd[`MUL_LAT];
-	  end
-	else if(r_do_msub[`MUL_LAT])
-	  begin
-	     y = r_mul[`MUL_LAT] - r_madd[`MUL_LAT];
-	  end
+	t_signed = opcode != MULTU;
+	y = r_mul31;
      end // always_comb
 
+   wire [63:0] w_pp_us[31:0];
    wire [63:0] w_pp[31:0];
+   logic [63:0] r_pp[`MUL_LAT:0][31:0];
    
+   //compute unsigned partial products
    generate
       for(genvar i = 0; i < 32; i=i+1)
 	begin
-	   assign w_pp[i] = { {(32-i){1'b0}}, (src_A & {32{src_B[i]}} ), {i{1'b0}} };
+	   assign w_pp_us[i] = { {(32-i){1'b0}}, (src_A & {32{src_B[i]}} ), {i{1'b0}} };
+	end
+      
+      for(genvar i = 1; i < 32; i=i+1)
+	begin
+	   assign w_pp[i] = {w_pp_us[i][63:(32+i)], (t_signed ? ~w_pp_us[i][31+i] : w_pp_us[i][31+i]), w_pp_us[i][(30+i):0]};
 	end
    endgenerate
+   assign w_pp[0] = {w_pp_us[0][63:33], (t_signed ? {1'b1, ~w_pp_us[0][31]} : {1'b0, w_pp_us[0][31]} ), w_pp_us[0][30:0]};
+   assign w_pp[31] = t_signed ? {1'b1, w_pp_us[31][62], ~w_pp_us[31][61:31], 31'd0} : w_pp_us[31];
 
+   always_ff@(posedge clk)
+     begin
+	r_pp[0] <= w_pp;
+	for(integer i = 1; i < `MUL_LAT; i=i+1)
+	  begin
+	     r_pp[i] <= r_pp[i-1];
+	  end
+     end
+
+   
    wire [63:0] w_c0, w_s0;
    csa #(.N(64)) c0 (.a(w_pp[0]), .b(w_pp[1]), .cin(w_pp[2]), .s(w_s0), .cout(w_c0));
    wire [63:0] w_c1, w_s1;
@@ -117,78 +141,58 @@ module mul(clk,reset,opcode,go,
    csa #(.N(64)) c10 (.a(w_s9), .b({w_c9[62:0], 1'b0}), .cin(w_pp[12]), .s(w_s10), .cout(w_c10));
    wire [63:0] w_c11, w_s11;
    csa #(.N(64)) c11 (.a(w_s10), .b({w_c10[62:0], 1'b0}), .cin(w_pp[13]), .s(w_s11), .cout(w_c11));
+
+   wire [63:0] r_s11, r_c11;
+   ff #(.N(64)) ff_s0 (.clk(clk), .d(w_s11), .q(r_s11));
+   ff #(.N(64)) ff_c0 (.clk(clk), .d(w_c11), .q(r_c11));
+      
    wire [63:0] w_c12, w_s12;
-   csa #(.N(64)) c12 (.a(w_s11), .b({w_c11[62:0], 1'b0}), .cin(w_pp[14]), .s(w_s12), .cout(w_c12));
+   csa #(.N(64)) c12 (.a(r_s11), .b({r_c11[62:0], 1'b0}), .cin(r_pp[0][14]), .s(w_s12), .cout(w_c12));
    wire [63:0] w_c13, w_s13;
-   csa #(.N(64)) c13 (.a(w_s12), .b({w_c12[62:0], 1'b0}), .cin(w_pp[15]), .s(w_s13), .cout(w_c13));
+   csa #(.N(64)) c13 (.a(w_s12), .b({w_c12[62:0], 1'b0}), .cin(r_pp[0][15]), .s(w_s13), .cout(w_c13));
    wire [63:0] w_c14, w_s14;
-   csa #(.N(64)) c14 (.a(w_s13), .b({w_c13[62:0], 1'b0}), .cin(w_pp[16]), .s(w_s14), .cout(w_c14));
+   csa #(.N(64)) c14 (.a(w_s13), .b({w_c13[62:0], 1'b0}), .cin(r_pp[0][16]), .s(w_s14), .cout(w_c14));
    wire [63:0] w_c15, w_s15;
-   csa #(.N(64)) c15 (.a(w_s14), .b({w_c14[62:0], 1'b0}), .cin(w_pp[17]), .s(w_s15), .cout(w_c15));
+   csa #(.N(64)) c15 (.a(w_s14), .b({w_c14[62:0], 1'b0}), .cin(r_pp[0][17]), .s(w_s15), .cout(w_c15));
    wire [63:0] w_c16, w_s16;
-   csa #(.N(64)) c16 (.a(w_s15), .b({w_c15[62:0], 1'b0}), .cin(w_pp[18]), .s(w_s16), .cout(w_c16));
+   csa #(.N(64)) c16 (.a(w_s15), .b({w_c15[62:0], 1'b0}), .cin(r_pp[0][18]), .s(w_s16), .cout(w_c16));
    wire [63:0] w_c17, w_s17;
-   csa #(.N(64)) c17 (.a(w_s16), .b({w_c16[62:0], 1'b0}), .cin(w_pp[19]), .s(w_s17), .cout(w_c17));
+   csa #(.N(64)) c17 (.a(w_s16), .b({w_c16[62:0], 1'b0}), .cin(r_pp[0][19]), .s(w_s17), .cout(w_c17));
    wire [63:0] w_c18, w_s18;
-   csa #(.N(64)) c18 (.a(w_s17), .b({w_c17[62:0], 1'b0}), .cin(w_pp[20]), .s(w_s18), .cout(w_c18));
+   csa #(.N(64)) c18 (.a(w_s17), .b({w_c17[62:0], 1'b0}), .cin(r_pp[0][20]), .s(w_s18), .cout(w_c18));
    wire [63:0] w_c19, w_s19;
-   csa #(.N(64)) c19 (.a(w_s18), .b({w_c18[62:0], 1'b0}), .cin(w_pp[21]), .s(w_s19), .cout(w_c19));
+   csa #(.N(64)) c19 (.a(w_s18), .b({w_c18[62:0], 1'b0}), .cin(r_pp[0][21]), .s(w_s19), .cout(w_c19));
    wire [63:0] w_c20, w_s20;
-   csa #(.N(64)) c20 (.a(w_s19), .b({w_c19[62:0], 1'b0}), .cin(w_pp[22]), .s(w_s20), .cout(w_c20));
+   csa #(.N(64)) c20 (.a(w_s19), .b({w_c19[62:0], 1'b0}), .cin(r_pp[0][22]), .s(w_s20), .cout(w_c20));
    wire [63:0] w_c21, w_s21;
-   csa #(.N(64)) c21 (.a(w_s20), .b({w_c20[62:0], 1'b0}), .cin(w_pp[23]), .s(w_s21), .cout(w_c21));
+   csa #(.N(64)) c21 (.a(w_s20), .b({w_c20[62:0], 1'b0}), .cin(r_pp[0][23]), .s(w_s21), .cout(w_c21));
    wire [63:0] w_c22, w_s22;
-   csa #(.N(64)) c22 (.a(w_s21), .b({w_c21[62:0], 1'b0}), .cin(w_pp[24]), .s(w_s22), .cout(w_c22));
+   csa #(.N(64)) c22 (.a(w_s21), .b({w_c21[62:0], 1'b0}), .cin(r_pp[0][24]), .s(w_s22), .cout(w_c22));
+
+   wire [63:0] r_s22, r_c22;
+   ff #(.N(64)) ff_s1 (.clk(clk), .d(w_s22), .q(r_s22));
+   ff #(.N(64)) ff_c1 (.clk(clk), .d(w_c22), .q(r_c22));
+   
    wire [63:0] w_c23, w_s23;
-   csa #(.N(64)) c23 (.a(w_s22), .b({w_c22[62:0], 1'b0}), .cin(w_pp[25]), .s(w_s23), .cout(w_c23));
+   csa #(.N(64)) c23 (.a(r_s22), .b({r_c22[62:0], 1'b0}), .cin(r_pp[1][25]), .s(w_s23), .cout(w_c23));
    wire [63:0] w_c24, w_s24;
-   csa #(.N(64)) c24 (.a(w_s23), .b({w_c23[62:0], 1'b0}), .cin(w_pp[26]), .s(w_s24), .cout(w_c24));
+   csa #(.N(64)) c24 (.a(w_s23), .b({w_c23[62:0], 1'b0}), .cin(r_pp[1][26]), .s(w_s24), .cout(w_c24));
    wire [63:0] w_c25, w_s25;
-   csa #(.N(64)) c25 (.a(w_s24), .b({w_c24[62:0], 1'b0}), .cin(w_pp[27]), .s(w_s25), .cout(w_c25));
+   csa #(.N(64)) c25 (.a(w_s24), .b({w_c24[62:0], 1'b0}), .cin(r_pp[1][27]), .s(w_s25), .cout(w_c25));
    wire [63:0] w_c26, w_s26;
-   csa #(.N(64)) c26 (.a(w_s25), .b({w_c25[62:0], 1'b0}), .cin(w_pp[28]), .s(w_s26), .cout(w_c26));
+   csa #(.N(64)) c26 (.a(w_s25), .b({w_c25[62:0], 1'b0}), .cin(r_pp[1][28]), .s(w_s26), .cout(w_c26));
    wire [63:0] w_c27, w_s27;
-   csa #(.N(64)) c27 (.a(w_s26), .b({w_c26[62:0], 1'b0}), .cin(w_pp[29]), .s(w_s27), .cout(w_c27));
+   csa #(.N(64)) c27 (.a(w_s26), .b({w_c26[62:0], 1'b0}), .cin(r_pp[1][29]), .s(w_s27), .cout(w_c27));
    wire [63:0] w_c28, w_s28;
-   csa #(.N(64)) c28 (.a(w_s27), .b({w_c27[62:0], 1'b0}), .cin(w_pp[30]), .s(w_s28), .cout(w_c28));
+   csa #(.N(64)) c28 (.a(w_s27), .b({w_c27[62:0], 1'b0}), .cin(r_pp[1][30]), .s(w_s28), .cout(w_c28));
    wire [63:0] w_c29, w_s29;
-   csa #(.N(64)) c29 (.a(w_s28), .b({w_c28[62:0], 1'b0}), .cin(w_pp[31]), .s(w_s29), .cout(w_c29));
+   csa #(.N(64)) c29 (.a(w_s28), .b({w_c28[62:0], 1'b0}), .cin(r_pp[1][31]), .s(w_s29), .cout(w_c29));
 
 
    wire [63:0] w_mul31 = w_s29 + {w_c29[62:0], 1'b0};
 
 
-   always_comb
-     begin
-	if(opcode == MULTU)
-	  begin
-	     //t_mul = w_mul[31];
-	     t_mul = src_A * src_B;
-	  end
-	else
-	  begin
-	     t_mul = $signed(src_A) * $signed(src_B);
-	  end
-     end // always_comb
-   
-   
-   reg [63:0] r_m, rr_m;
-   reg 	      r_p;
-      always_ff@(posedge clk)
-     begin
-	r_m <= w_mul31;
-	rr_m <= r_m;
-	r_p <= reset ? 1'b0 : (opcode == MULTU);
-     end
-
-   always_ff@(negedge clk)
-     begin
-	if(r_p && (r_m != r_mul[0]))
-	  begin
-	     $display("r_m = %x, r_mul[1] = %x", r_m, r_mul[0]);
-	     $stop();
-	  end
-     end
+   ff #(.N(64)) s0 (.clk(clk), .d(w_mul31), .q(r_mul31));
    
    always_ff@(posedge clk)
      begin
@@ -196,7 +200,6 @@ module mul(clk,reset,opcode,go,
 	  begin
 	     for(integer i = 0; i <= `MUL_LAT; i=i+1)
 	       begin
-		  r_mul[i] <= 'd0;
 		  r_rob_ptr[i] <= 'd0;
 		  r_gpr_ptr[i] <= 'd0;
 		  r_hilo_ptr[i] <= 'd0;
@@ -214,7 +217,6 @@ module mul(clk,reset,opcode,go,
 	       begin
 		  if(i == 0)
 		    begin
-		       r_mul[0] <= t_mul;
 		       r_do_madd[0] <= go & (opcode == MADD);
 		       r_do_msub[0] <= go & (opcode == MSUB);
 		       r_complete[0] <= go;
@@ -227,7 +229,6 @@ module mul(clk,reset,opcode,go,
 		    end
 		  else
 		    begin
-		       r_mul[i] <= r_mul[i-1];
 		       r_do_madd[i] <= r_do_madd[i-1];
 		       r_do_msub[i] <= r_do_msub[i-1];
 		       r_complete[i] <= r_complete[i-1];
