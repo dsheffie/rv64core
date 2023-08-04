87b588ae7c5f1431dfccc9ebe2f07f48bf1f224b
David Sheffield <sheffield.david@gmail.com>
2023-08-03 21:43:41 -0700

    use o3, script to generate parallel run

---
+++ csmith/run_parallel.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import subprocess
+import glob
+import re
+
+if __name__ == '__main__':
+    tests = glob.glob('*.rv32')
+    passed = 0
+
+    dsheffie = {}
+    dsheffie_icnt = {}
+    spike = {}
+    
+    min_icnt = 1000000000
+    min_icnt_test = None
+
+    with open('jobs.txt', 'w') as o:
+        for test in tests:
+            o.write('../rv32_core -f %s --maxicnt %d &> %s.out\n' % (test, 16*1024*1024, test)) 
+
+
