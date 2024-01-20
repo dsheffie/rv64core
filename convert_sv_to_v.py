30fec05b6619b013e89eb79b93faf13aaca4aec4
David Sheffield <sheffield.david@gmail.com>
2024-01-20 07:58:24 -0800

    concatenate converted output into single file

--- convert_sv_to_v.py
+++ convert_sv_to_v.py
@@ -8,12 +8,21 @@ def main():
     svs = glob.glob('*.sv')
     if not os.path.isdir('verilog'):
         os.mkdir('verilog')
-        
+
+    outputs = []
     for sv in svs:
         r = sv.split('.sv')[0]
         v = r+'.v'
         cmd = ['sv2v', sv, '--write=verilog/'+v]
+        outputs.append('verilog/' + v)
         subprocess.run(cmd)
+
+    with open('rv32core.v', 'w') as o:
+        for output in outputs:
+            with open(output, 'r') as in_:
+                for line in in_:
+                    o.write('%s' % line)
+                o.write('\n\n')
 
 if __name__ == "__main__":
     main()
